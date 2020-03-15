4416056bb3e18350bde1b6fa5298e52e6ce56ebf
Alexandra Ackerman <aaackerman@Alexandras-MBP.home>
2020-03-15 18:01:10 -0400

    remove recent work

--- src/pages/index.js
+++ src/pages/index.js
@@ -54,24 +54,23 @@ class HomeIndex extends React.Component {
                         </ul>
                     </section>
 
-                    <section id="two">
-                        <h2>Recent Work</h2>
-
-                        <Gallery images={DEFAULT_IMAGES.map(({ id, source, thumbnail, caption, description }) => ({
-                            source,
-                            thumbnail,
-                            caption,
-                            description
-                        }))} />
-
-                        <ul className="actions">
-                            <li><a href="#" className="button">Full Portfolio</a></li>
-                        </ul>
-                    </section>
+                    {/* <section id="two"> */}
+                    {/*     <h2>Recent Work</h2> */}
+                    {/*  */}
+                    {/*     <Gallery images={DEFAULT_IMAGES.map(({ id, source, thumbnail, caption, description }) => ({ */}
+                    {/*         source, */}
+                    {/*         thumbnail, */}
+                    {/*         caption, */}
+                    {/*         description */}
+                    {/*     }))} /> */}
+                    {/*  */}
+                    {/*     <ul className="actions"> */}
+                    {/*         <li><a href="#" className="button">Full Portfolio</a></li> */}
+                    {/*     </ul> */}
+                    {/* </section> */}
 
                     <section id="three">
                         <h2>Get In Touch</h2>
-                        <p>Accumsan pellentesque commodo blandit enim arcu non at amet id arcu magna. Accumsan orci faucibus id eu lorem semper nunc nisi lorem vulputate lorem neque lorem ipsum dolor.</p>
                         <div className="row">
                             <div className="8u 12u$(small)">
                                 <form method="post" action="#">
