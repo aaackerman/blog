649f056ccae9a40d9d251a4da34e488fab976f74
aaackerman <aaackerman.dev@gmail.com>
2020-04-01 12:07:57 -0400

    update copy and links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,19 +1,43 @@
 import React from 'react'
 
 class Footer extends React.Component {
-    render() {
-        return (
-            <div id="footer">
-                <div className="inner">
-                    <ul className="icons">
-                        <li><a target="_blank" href="https://www.github.com/aaackerman" className="icon fa-github"><span className="label">Github</span></a></li>
-                        <li><a target="_blank" href="https://wwww.twitter.com/alexiconz" className="icon fa-twitter"><span className="label">Twitter</span></a></li>
-                        <li><a target="_blank" href="https://wwww.twitch.tv/alexiconz" className="icon fa-twitch"><span className="label">Twitch</span></a></li>
-                    </ul>
-                </div>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div id="footer">
+        <div className="inner">
+          <ul className="icons">
+            <li>
+              <a
+                target="_blank"
+                href="https://www.github.com/aaackerman"
+                className="icon fa-github"
+              >
+                <span className="label">Github</span>
+              </a>
+            </li>
+            <li>
+              <a
+                target="_blank"
+                href="https://twitter.com/alexiconz"
+                className="icon fa-twitter"
+              >
+                <span className="label">Twitter</span>
+              </a>
+            </li>
+            <li>
+              <a
+                target="_blank"
+                href="https://twitch.tv/alexiconz"
+                className="icon fa-twitch"
+              >
+                <span className="label">Twitch</span>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default Footer
--- src/components/Header.js
+++ src/components/Header.js
@@ -4,17 +4,21 @@ import Footer from './Footer'
 import avatar from '../assets/images/avatar.jpg'
 
 class Header extends React.Component {
-    render() {
-        return (
-            <header id="header">
-                <div className="inner">
-                    <a href="#" className="image avatar"><img src={avatar} alt="" /></a>
-                    <h1><strong>I'm Alex,</strong> web developer, educator, Twitch streamer and gym-nerd.</h1>
-                </div>
-                <Footer />
-            </header>
-        )
-    }
+  render() {
+    return (
+      <header id="header">
+        <div className="inner">
+          <a href="#" className="image avatar">
+            <img src={avatar} alt="" />
+          </a>
+          <h1>
+            <strong>Hello,</strong> I'm Alex.
+          </h1>
+        </div>
+        <Footer />
+      </header>
+    )
+  }
 }
 
 export default Header
